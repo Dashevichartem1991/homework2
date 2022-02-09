4410a2005e20e3509b1893fbb24df76ce70e72f0
Dashevichartem1991 <dashevichartem@gmail.com>
2022-02-09 23:18:15 +0300

    add task 9

---
+++ src/TaskNine.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskNine {
+    public static void main(String[] args) {
+        String s = " test adkfjkjdjksklccca accccca 0000-1111-2222-3333 wlkf;jw jklwflw;; 3424jkcccc wefjkkklwj cc aa";
+        String keyWord = "test";
+
+        Pattern pattern = Pattern.compile("\\b" + keyWord + "\\b");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()){
+
+            System.out.println("Слово в строке найдено, индекс первого символа равен " + matcher.start());
+
+        }
+    }
+}
