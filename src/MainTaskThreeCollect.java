eca811273a24e13edcea103e20359d6b9aec2fd1
Dashevichartem1991 <dashevichartem@gmail.com>
2022-02-11 00:57:30 +0300

    changed tasks 3

--- src/MainTaskThreeCollect.java
+++ src/MainTaskThreeCollect.java
@@ -1,13 +1,19 @@
 public class MainTaskThreeCollect {
     public static void main(String[] args) {
-        TaskThreeCollect x = new TaskThreeCollect(15);
+        TaskThreeCollect x = new TaskThreeCollect(5);
         System.out.println(x.getCount() + " " + x.getSomeNumber());
 
-        x.addNumber(12);
-        x.addNumber(11);
-        x.addNumber(11);
 
-        System.out.println(x.getCount() + " " + x.getSomeNumber());
+        x.addNumber(10);
+        x.addNumber(10);
+        x.addNumber(10);
+        x.addNumber(10);
+        x.addNumber(10);
+        x.addNumber(40);
+
+
+
+        System.out.println(x.getCount() + " " + x.getSum());
 
         x.calculating();
 
--- src/TaskThreeCollect.java
+++ src/TaskThreeCollect.java
@@ -1,20 +1,39 @@
 public class TaskThreeCollect {
     private Integer someNumber;
-    private Integer count = 1;
+    private Integer count = 0;
+    private Integer[] memory = new Integer[5];
+    private Integer sum;
+
 
 
     TaskThreeCollect(Integer someNumber){
         this.someNumber = someNumber;
-        this.count = count;
+
+
     }
 
     public Integer addNumber(Integer number){
-        someNumber += number;
         count++;
+
+        if(count <= 4) {
+            memory[count] = number;
+        }else{
+            count = 0;
+            memory[count] = number;
+
+        }
+
+
         return null;
     }
     public void calculating(){
-        System.out.println("Среднее значение = " + (Double.valueOf(someNumber) / Double.valueOf(count)));
+        sum = 0;
+        for (int i = 0; i < memory.length; i++) {
+
+            sum += memory[i];
+            System.out.println(sum);
+        }
+        System.out.println("Среднее значение = " + (Double.valueOf(sum) / 5));
 
     }
 
@@ -24,6 +43,14 @@ public class TaskThreeCollect {
 
     public void setSomeNumber(Integer someNumber) {
         this.someNumber = someNumber;
+    }
+
+    public Integer[] getMemory() {
+        return memory;
+    }
+
+    public Integer getSum() {
+        return sum;
     }
 
     public Integer getCount() {
